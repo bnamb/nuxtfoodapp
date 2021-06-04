c5bd588e21146fa1f71bad822e272ef0fdcbb1dc
bnamb <nambo.brenda@gmail.com>
2021-06-03 22:37:16 -0400

    initial commit

---
+++ components/AppFooter.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+<footer>&#169; MakerPaulBootCamp - Made with &hearts; by Brenda</footer>
+  </div>
+</template>
---
+++ pages/restaurants.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Restaurant List</h1>
+    <AppRestaurantInfo />
+  </div>
+</template>
+
+<script>
+  import AppRestaurantInfo from '@/components/AppRestaurantInfo.vue';
+
+  export default {
+    components: {
+      AppRestaurantInfo
+    },
+  };
+</script>
+
+<style lang="scss" scoped>
+h1 {
+  text-align: center;
+  padding: 30px;
+  font-size: 5vmax;
+}
+</style>
